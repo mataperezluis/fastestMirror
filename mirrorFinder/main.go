3419deab6586aa5d703d0c2b66468ec5866859b5
mataperezluis <mata.perez.luis@gmail.com>
2020-10-06 09:28:10 -0300

    first commit

--- mirrorFinder/main.go
+++ mirrorFinder/main.go
@@ -3,7 +3,7 @@ package mirrorFinder
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/mataperezluis/fastestMirror/mirrorFinder/"
+	"github.com/mataperezluis/fastestMirror/mirrorFinder"
 	"log"
 	"net/http"
 	"time"
